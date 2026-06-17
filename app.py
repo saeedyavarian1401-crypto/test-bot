139ce502a7d236536292b0a58df01f4ed9ad8f7d
saeedyavarian1401-crypto <saeedyavarian.1401@gmail.com>
2026-06-17 13:34:44 +0330

    Update app.py

--- app.py
+++ app.py
@@ -876,3 +876,6 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     print(f"🚀 ربات روی پورت {port} اجرا شد")
     app.run(host='0.0.0.0', port=port, debug=False)
+@app.route('/')
+def home():
+    return "ربات فعال است", 200
